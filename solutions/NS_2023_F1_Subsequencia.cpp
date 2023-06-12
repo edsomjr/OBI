f564ea13ffc97f47d39a059fbf1f12e5e8ae1a60
Edson Alves <edsomjr@gmail.com>
2023-06-12 15:54:40 -0300

    Solução do problema NS 2023 F1 Sênior - Subsequência.

---
+++ solutions/NS_2023_F1_Subsequencia.cpp
@@ -0,0 +1,40 @@
+// OBI 2023 - Fase 1 - Nível Sênior: Subsequência (https://olimpiada.ic.unicamp.br/static/extras/obi2023/provas/ProvaOBI2023_f1ps.pdf)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char solve(int B, const vector<int>& xs, const vector<int>& ys)
+{
+    int i = 0;
+
+    for (auto x : xs)
+    {
+        if (ys[i] == x)
+            ++i;
+
+        if (i == B)
+            return 'S';
+    }
+
+    return 'N';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    int A, B;
+    cin >> A >> B;
+
+    vector<int> xs(A), ys(B);
+
+    for (int i = 0; i < A; ++i)
+            cin >> xs[i];
+
+    for (int i = 0; i < B; ++i)
+            cin >> ys[i];
+
+    cout << solve(B, xs, ys) << '\n';
+
+    return 0;
+}
