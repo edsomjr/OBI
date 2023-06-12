7a33006af886f83ff131a01990a9e087d8d1b452
Edson Alves <edsomjr@gmail.com>
2023-06-12 14:11:47 -0300

    Solução do problema NS 2023 F1 Contas a pagar.

---
+++ solutions/NS_2023_F1_Contas_a_pagar.cpp
@@ -0,0 +1,28 @@
+// OBI 2023 - Fase 1 - Nível Sênior: Contas a pagar (https://olimpiada.ic.unicamp.br/static/extras/obi2023/provas/ProvaOBI2023_f1ps.pdf)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int V, int A, int F, int P)
+{
+    auto S = A + F + P;
+
+    if (V >= S)
+        return 3;
+    else if (V >= A + F or V >= A + P or V >= F + P)
+        return 2;
+    else if (V >= A or V >= F or V >= P)
+        return 1;
+
+    return 0;
+}
+
+int main()
+{
+    int V, A, F, P;
+    cin >> V >> A >> F >> P;
+
+    cout << solve(V, A, F, P) << '\n';
+
+    return 0;
+}
